1962dfc950efae52d2ac50fb8c5871b59837e0d1
Kiana's Kaheru Choco Parfait <40626713+KianaKaslana638@users.noreply.github.com>
2021-12-16 14:39:14 +0800

    Add files via upload

---
+++ download-yt-audio.ps1
@@ -0,0 +1,35 @@
+# Script by Kiana's Kaheru Choco Parfait
+
+$videoURL = $args[0]
+
+
+<#  Calls the yt-dlp with options specified below.
+
+    Nomenclature of arguments and options:
+    --external-downloader: 
+        specifies what downloader yt-dlp will use.
+    --external-downloader-args: 
+        specifies what arguments yt-dlp will pass to the external downloader.
+    --format bestaudio[ext=m4a]:
+        automatically selects the best audio format possible.
+    --output "%(uploader)s - %(title)s - %(upload_date)s.%(ext)s":
+        Once the download process has finished, yt-dlp will rename the file
+        according to the specified format.
+    --no-part:
+        yt-dlp will not download the audio into multiple parts.
+    --add-metadata:
+        metadata will be embedded to the final downloaded audio.
+    --write-description:
+        writes the audio's description to a separate text file.
+#>
+
+yt-dlp `
+    --external-downloader aria2c `
+    --external-downloader-args `
+        "--continue --split=8 --max-connection-per-server=8 --min-split-size=1M"`
+    --format bestaudio[ext=m4a] `
+    --output "%(uploader)s - %(title)s - %(upload_date)s.%(ext)s" `
+    --no-part `
+    --add-metadata `
+    --write-description `
+    $videoURL
